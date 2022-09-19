6af4d20f9371d7b3cbf02ffd6f5dd4570dcad0c2
Daniel Sandberg <bjorndaniel@gmail.com>
2022-09-19 12:36:42 +0200

    Added option to keep certain values when replacing

--- src/Data/ReplaceObject.cs
+++ src/Data/ReplaceObject.cs
@@ -6,5 +6,10 @@ public class ReplaceObject
     public string ColumnName { get; set; } = "";
     public string ReplaceType { get; set; } = "";
     public string TableNameToSql() => TableName.Split(".").Aggregate((a, b) => $"[{a}].[{b}]");
-    public List<string> ValuesToKeep { get; set; } = new();
+    public List<ItemToKeep> ValuesToKeep { get; set; } = new();
+}
+
+public class ItemToKeep
+{
+    public string Value { get; set; } = "";
 }
